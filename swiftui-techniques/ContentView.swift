3fa3d4bfc832d9ed81e592649b6de5c480450aac
Andy Stechishin <andy.stechishin@gmail.com>
2020-01-11 15:21:53 -0700

    Initial Commit

---
+++ swiftui-techniques/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-techniques
+//
+//  Created by Andy Stechishin on 2020-01-11.
+//  Copyright © 2020 Andy Stechishin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
